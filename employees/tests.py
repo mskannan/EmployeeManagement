472d9c5537fba886f7286a957c454fd8626e3a36
mskannan <senthkannan55@gmail.com>
2025-02-04 13:04:11 +0530

    Email Validation Testcase added

--- employees/tests.py
+++ employees/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from employees.models import Employee
+from django.db.utils import IntegrityError
 
 class EmployeeModelTest(TestCase):
 
@@ -17,5 +18,26 @@ class EmployeeModelTest(TestCase):
         self.assertEqual(employee.name, "John Doe")
         self.assertEqual(employee.email, "john.doe@example.com")
         self.assertEqual(employee.phone, "1234567890")
-        self.assertEqual(employee.username, "john1234")
-        self.assertTrue(employee.check_password("Password@123"))
+        self.assertEqual(employee.username, "john1234") 
+
+
+    # Test case to check email uniqueness
+    def test_employee_email_unique(self):
+        # Create the first employee with a unique email
+        Employee.objects.create(
+            name="kannan",
+            email="kannan@mail.in",
+            phone="9876543210",
+            username="kannan1234",
+            password="Password@123"
+        )
+        # Check same email raises an IntegrityError
+        with self.assertRaises(IntegrityError):
+            Employee.objects.create(
+                name="senthamarai",
+                email="kannan@mail.in",  # Same email as kannan
+                phone="1112233445",
+                username="sentha12345",
+                password="Password@123"
+            )
+ 
